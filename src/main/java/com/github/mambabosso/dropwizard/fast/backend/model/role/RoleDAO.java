7849d6a5824e54cf1bafed62f4e746c3e8808662
Luis Weich <mambabosso@gmail.com>
2020-04-28 21:17:51 +0200

    Update

--- src/main/java/com/github/mambabosso/dropwizard/fast/backend/model/role/RoleDAO.java
+++ src/main/java/com/github/mambabosso/dropwizard/fast/backend/model/role/RoleDAO.java
@@ -1,12 +1,26 @@
 package com.github.mambabosso.dropwizard.fast.backend.model.role;
 
 import com.github.mambabosso.dropwizard.fast.backend.dao.BaseDAO;
+import lombok.NonNull;
 import org.hibernate.SessionFactory;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class RoleDAO extends BaseDAO<Role> {
 
+    private final QRole role = QRole.role;
+
     public RoleDAO(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    public UUID create(Role role) {
+        return (UUID)save(role);
+    }
+
+    public Optional<Role> getRoleByName(@NonNull String name) {
+        return Optional.ofNullable(query().select(role).from(role).where(role.name.eq(name)).fetchFirst());
     }
 
 }
--- src/main/java/com/github/mambabosso/dropwizard/fast/backend/model/user/UserDAO.java
+++ src/main/java/com/github/mambabosso/dropwizard/fast/backend/model/user/UserDAO.java
@@ -1,12 +1,26 @@
 package com.github.mambabosso.dropwizard.fast.backend.model.user;
 
 import com.github.mambabosso.dropwizard.fast.backend.dao.BaseDAO;
+import lombok.NonNull;
 import org.hibernate.SessionFactory;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class UserDAO extends BaseDAO<User> {
 
+    private final QUser user = QUser.user;
+
     public UserDAO(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    public UUID create(User user) {
+        return (UUID)save(user);
+    }
+
+    public Optional<User> getUserByName(@NonNull String name) {
+        return Optional.ofNullable(query().select(user).from(user).where(user.name.eq(name)).fetchFirst());
     }
 
 }
