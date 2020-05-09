7627f0a3c06a190d005786c44e4d850b89b84bca
Luis Weich <mambabosso@gmail.com>
2020-05-09 16:41:20 +0200

    Update

--- src/test/java/com/github/mambabosso/dfb/model/token/TokenDAOTest.java
+++ src/test/java/com/github/mambabosso/dfb/model/token/TokenDAOTest.java
@@ -1,8 +1,9 @@
-package com.github.mambabosso.dfb.model.role;
+package com.github.mambabosso.dfb.model.token;
 
 import io.dropwizard.testing.junit5.DAOTestExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -10,17 +11,17 @@ import java.util.UUID;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(DropwizardExtensionsSupport.class)
-public class RoleDAOTest {
+public class TokenDAOTest {
 
     private static final DAOTestExtension daoTestRule = DAOTestExtension.newBuilder()
-            .addEntityClass(Role.class)
+            .addEntityClass(Token.class)
             .build();
 
-    private static RoleDAO dao;
+    private static TokenDAO dao;
 
     @BeforeAll
     public static void init() {
-        dao = new RoleDAO(daoTestRule.getSessionFactory());
+        dao = new TokenDAO(daoTestRule.getSessionFactory());
     }
 
     @Test
@@ -30,15 +31,15 @@ public class RoleDAOTest {
         DateTime now = DateTime.now();
 
         UUID id = daoTestRule.inTransaction(() -> {
-            return dao.insert(Role.builder().name("role").createdAt(now).build());
+            return dao.insert(Token.builder().value("token").type(TokenTypes.JWT).expiresAt(now).lastAccess(now).createdAt(now).build());
         });
 
         Assertions.assertEquals(1, daoTestRule.inTransaction(() -> {
-            return dao.update(id, Role.builder().name("xxxx").build());
+            return dao.update(id, Token.builder().lastAccess(now.plus(Duration.standardDays(2))).build());
         }));
 
-        Assertions.assertEquals("xxxx", daoTestRule.inTransaction(() -> {
-            return dao.getById(id).getName();
+        Assertions.assertEquals(now.plus(Duration.standardDays(2)), daoTestRule.inTransaction(() -> {
+            return dao.getById(id).getLastAccess();
         }));
 
         Assertions.assertEquals(1, daoTestRule.inTransaction(() -> {
