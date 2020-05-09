cd9fb5d213ffacdd87820e4bb47cceccdeebdc00
Luis Weich <mambabosso@gmail.com>
2020-05-09 19:08:03 +0200

    Update

--- src/main/java/com/github/mambabosso/dfb/model/token/TokenTypes.java
+++ src/main/java/com/github/mambabosso/dfb/model/token/TokenTypes.java
@@ -2,7 +2,7 @@ package com.github.mambabosso.dfb.model.token;
 
 public enum TokenTypes {
 
-    JWT("jwt"), JWT_ID("jwt_id"), SESSION("session"), SESSION_ID("session_id"), CUSTOM("custom");
+    JWT("jwt"), JWT_ID("jwt_id"), SESSION("session"), SESSION_ID("session_id"), ID("id"), CUSTOM("custom");
 
     private final String tokenType;
 
